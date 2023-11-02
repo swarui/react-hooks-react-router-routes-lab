a2bf5bd3f23ea33b3ddc7b8fb0253f39a10b1349
swarui <stevewaruim@gmail.com>
2023-11-02 08:37:45 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,42 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        activeStyle={{
+          display: "block",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="movies"
+        activeStyle={{
+          display: "block",
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="directors"
+        activeStyle={{
+          display: "block",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        activeStyle={{
+          display: "block",
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
